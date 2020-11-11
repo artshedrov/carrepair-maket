d08384f2abbd146182764d6c50aab57403786238
artschedrov <art.worknsk@gmail.com>
2020-11-12 00:25:16 +0700

    start header

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const style = require('./style');
-const pug2html = require('./pug2html');
 const html = require('./html')
 const scripts = require('./scripts');
 const image = require('./image');
@@ -12,7 +11,7 @@ module.exports = function serve() {
     server: { baseDir: 'build/'},
     notify: false
   });
-  gulp.watch('src/styles/**/*.scss', gulp.series(style)).on('change', server.reload)
+  gulp.watch('src/css/*.scss', gulp.series(style)).on('change', server.reload)
   //gulp.watch('src/pages/**/*.pug', gulp.series(pug2html))
   gulp.watch('src/*.html', gulp.series(html))
   gulp.watch('src/img/*.{png,jpg,svg}', gulp.series(image)).on('change', server.reload)
--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -1,12 +1,12 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const scss = require('gulp-sass');
 const concat = require('gulp-concat');
 const cleanCss = require('gulp-clean-css');
 
 module.exports = function style() {
-  return gulp.src('src/styles/*.scss')
-    .pipe(sass())
-    .pipe(concat('style.css'))
+  return gulp.src(['src/css/*.scss'])
+    .pipe(scss())
+    //.pipe(concat('style.css'))
     .pipe(cleanCss())
     .pipe(gulp.dest('build/css'))
 }
