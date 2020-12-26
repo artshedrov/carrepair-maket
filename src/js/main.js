493b00948794ab91ab44a17b41aa2154c8a80b8f
artschedrov <art.worknsk@gmail.com>
2020-12-27 04:18:05 +0700

    fix dropdown menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
 let arrow = document.querySelectorAll('.menu__arrow');
-//let submenu = document.querySelectorAll('.sub-menu__list');
 
-for (i=0; i < arrow.length; i++) {
-  let thisLink = arrow[i].previousElementSibling;
-  let subMneu = arrow[i].nextElementSibling;
+for (i = 0; i < arrow.length; i++) {
+  let subMenu = arrow[i].nextElementSibling;
   let thisArrow = arrow[i];
+  
   arrow[i].addEventListener('click', () => {
     thisArrow.classList.toggle('menu__arrow--active');
-  //submenu.classList.toggle('sub-menu__list--active');
+    subMenu.classList.toggle('sub-menu__list--active');
   });
-}; 
+};
